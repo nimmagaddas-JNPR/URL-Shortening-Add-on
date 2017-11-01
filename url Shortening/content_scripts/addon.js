7513cc55aed311f1f7c0bc2e962b08bdae6076d8
sriramya04 <ramya.nimmagadda77@gmail.com>
2017-10-31 20:07:27 -0700

    addon

--- url Shortening/content_scripts/addon.js
+++ url Shortening/content_scripts/addon.js
@@ -1,20 +1,10 @@
 function shorten(request, sender, sendResponse) {
-  removeEverything();
-  display(request.output);
+  display(document.URL);
   browser.runtime.onMessage.removeListener(shorten);
 }
 
-function display(name){
-var para = document.createElement("P");                       
-var t = document.createTextNode(name);       
-para.appendChild(t);                                          
-document.body.appendChild(para); 
-}
-
-function removeEverything() {
-  while (document.body.firstChild) {
-    document.body.firstChild.remove();
-  }
+function display(pageURL) {
+  browser.runtime.sendMessage(pageURL);
 }
 
 browser.runtime.onMessage.addListener(shorten);
--- url Shortening/popup/shorten.js
+++ url Shortening/popup/shorten.js
@@ -1,34 +1,33 @@
-
-//  function makeShort(){
-//     var long = document.getElementById("url-field").value;
-//     var res = long.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-//     if(res==null){
-//       document.getElementById("output").innerHTML= "Not Valid URL"
-//     }
-//     else{
-//       var short = encode(long);
-//       var output = short.link(long);
-//       document.getElementById("output").innerHTML = output;
-//     }
-// }
+var urlToCode = {}, codeToUrl = {};
 
 document.addEventListener("click", (e)=>{
     var link = e.target.textContent;
-    var resultURL = browser.extension.getURL("link");
     browser.tabs.executeScript({ 
       file: "/content_scripts/addon.js" 
     });
+
+    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+    gettingActiveTab.then((tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, {msg: 'Message sent!'});
+    });
 });
 
-var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-gettingActiveTab.then((tabs) => {
-      browser.tabs.sendMessage(
-          tab.id,
-          tab.url,
-          {output: resultURL});
-  });
+browser.runtime.onMessage.addListener(encode);
 
-var urlToCode = {}, codeToUrl = {};
+function encode(pageURL) {
+  
+  // check if miniurl already exists 
+    while (urlToCode[pageURL] === undefined) {
+        let code = getRandomCode(6);
+        if (codeToUrl[code] === undefined) {
+            codeToUrl[code] = pageURL;
+            urlToCode[pageURL] = code;
+        }
+    }
+    const shortURL = `http://miniurl.com/${urlToCode[pageURL]}`;
+    document.getElementById("output").innerHTML = shortURL.link(pageURL);
+}
+
 // generate 6 random digit code
 function getRandomCode(len) {
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -38,24 +37,3 @@ function getRandomCode(len) {
     }
     return results;
 }
-
-var encode = function(longUrl) {
-    //check if miniurl already exists 
-    while (urlToCode[longUrl] === undefined) {
-        let code = getRandomCode(6);
-        if (codeToUrl[code] === undefined) {
-            codeToUrl[code] = longUrl;
-            urlToCode[longUrl] = code;
-        }
-    }
-    return "http://miniurl.com/" + urlToCode[longUrl];
-};
-
-var lng = window.location.href;
-var short = encode(lng);
-      var output = short.link(lng);
-      document.getElementById("output").innerHTML = output;
-
-
- 
- 
